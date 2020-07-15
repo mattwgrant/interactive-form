af7d4a346718375a16582101387d5ba779d98784
Matthew Grant <amattwgrant@gmail.com>
2020-07-15 19:28:56 -0400

    working on function to call createInput when select value equals other

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-function focus() {
-	/*** 
-		this function selects the input with the ID of name
+/*** 
+		This function selects the input with the ID of name
 		and puts the focus there so that the user can start 
 		typing their name as soon as the page loads, without
 		having to manually go to the field
 	***/
+function focus() {
 	document.getElementById('name').focus();
 }
 focus();
@@ -15,52 +15,58 @@ focus();
 ***/
 
 function hideOtherInput() {
-	// this variable selects the first fieldset element
+	// This variable selects the first fieldset element
 	let fieldSet = document.querySelector('fieldset');
-	// this element selects the text input
+	// This element selects the text input
 	let otherInput = document.querySelector('#other-title');
 	/***
-	this calls remove child on the fieldSet variable
-	and removes the otherInput so that it does not display if JS
-	is working but allows the field to display if JS is mnot working
+		This calls remove child on the fieldSet variable
+		and removes the otherInput so that it does not display if JS
+		is working but allows the field to display if JS is not working
 	***/
 	fieldSet.removeChild(otherInput);
 }
 // This calls it so that the function runs automatically to hide the element
 hideOtherInput();
 
+
+/***
+	This function creates the text input field for the 'Other'
+	job role
+***/
+
+function createInput() {
+	// this variable selects the first fieldset element
+	let title = document.querySelector('fieldset');
+	// this creates the text input for the other job role
+	let otherInput = document.createElement('INPUT');
+	// here I assign it an ID of other title
+	otherInput.id = 'other-title';
+	// this sets its type to tex
+	otherInput.type = 'text';
+	// this gives it a placeholder value
+	otherInput.placeholder = 'Your Job Role';
+	// this appends it to the end of the fieldset taht was selected
+	title.appendChild(otherInput);
+};
+
+
+/***
+	This function calls createInput if the 'Other' job role is 
+	selected
+***/
+
 function showOtherInput() {
+	let select = document.querySelector('select');
+	// let other = document.querySelector('option[value="other"]');
+	if ( select.value === 'other' ) {
+		createInput();
+	} 
+};
 
-}
 
 
-/***
-	This function appends a field to enter more information about
-	the job title the user is interested in if they select other.
-***/
-// function jobRole() {
-	// this variable selects the element with the id of title
-	// let title = document.querySelector('#title');
-	/***
-		If the title they choose is other, it will append a text
-		input field for the user to enter more information in to,.
-	***/
-// 	if ( 'option[value="other"]' )
-// 		let otherInput = document.createElement('INPUT');
-// 		otherInput.id = 'other-title';
-// 		otherInput.type = 'text';
-// 		title.appendChild
-// }
 
-/***
-	if the user selection is other, append a text input below the 
-***/
-	function createInput() {
-		let title = document.querySelector('fieldset');
-		let otherInput = document.createElement('INPUT');
-		otherInput.id = 'other-title';
-		otherInput.type = 'text';
-		otherInput.placeholder = 'Your Job Role';
-		title.appendChild(otherInput);
-	};
-	createInput();
+
+	
+	
